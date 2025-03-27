113d11f467b7b568d177d35fe873d9932896b69f
James Thurley <james@thurley.com>
2025-03-27 20:07:36 +0000

    Initial implementation

---
+++ jsx_samples/image.jsx
@@ -0,0 +1,18 @@
+<div
+  style={{
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    flexWrap: 'nowrap',
+    backgroundColor: 'white',
+    backgroundImage: 'radial-gradient(circle at 25px 25px, lightgray 2%, transparent 0%), radial-gradient(circle at 75px 75px, lightgray 2%, transparent 0%)',
+    backgroundSize: '100px 100px',
+  }}
+>
+  <img src="https://picsum.photos/150/100" width={150} height={100} />
+  <b>Images</b>
+</div>
