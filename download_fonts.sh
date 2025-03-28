0a9f6702268ec83ef5ac21a246b4bdc0de13ade5
James Thurley <james@thurley.com>
2025-03-28 19:42:45 +0000

    Add scripts to download test fonts

---
+++ download_fonts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p fonts
+
+./download_font.sh "./fonts" "Doto.ttf" "https://github.com/google/fonts/raw/refs/heads/main/ofl/doto/Doto%5BROND,wght%5D.ttf"
+./download_font.sh "./fonts" "Inter.ttf" "https://github.com/google/fonts/raw/refs/heads/main/ofl/inter/Inter%5Bopsz,wght%5D.ttf"
