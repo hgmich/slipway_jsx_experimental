faf34ab8ba0c1d55f9cc1e0e26df78f2ff8ff32c
James Thurley <james@thurley.com>
2025-03-28 08:19:04 +0000

    Improve fonts and data binding support

--- jsx_samples/data_binding.jsx
+++ jsx_samples/data_binding.jsx
@@ -10,9 +10,20 @@
     flexWrap: 'nowrap',
     backgroundColor: 'white',
     backgroundImage: 'radial-gradient(circle at 25px 25px, lightgray 2%, transparent 0%), radial-gradient(circle at 75px 75px, lightgray 2%, transparent 0%)',
-    backgroundSize: '100px 100px'
+    backgroundSize: '100px 100px',
   }}
 >
-  <img src="https://picsum.photos/150/100" width={150} height={100} />
-  <b style={{ fontFamily: 'monospace', fontSize: '16px' }}>Images</b>
+  <div
+    style={{
+      display: 'flex',
+      fontSize: 40,
+      fontStyle: 'normal',
+      color: 'black',
+      marginTop: 30,
+      lineHeight: 1.8,
+      whiteSpace: 'pre-wrap',
+    }}
+  >
+    <b>data.foo: {data.foo}</b>
+  </div>
 </div>
